10b6cccec7b8ce7ac8053777ff6315c1be04e3ed
MLalithambigai <155508325+MLalithambigai@users.noreply.github.com>
2024-01-06 11:19:49 +0530

    Update functions.h

--- Headers/functions.h
+++ Headers/functions.h
@@ -1,4 +1,5 @@
 // T.F. Nusha - 21/ENG/138
+// M. Lalithambigai - 21/ENG/075
 
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
@@ -8,8 +9,8 @@
 #include <fstream>
 #include <sstream>
 using namespace std;
-// Function declarations (prototypes)
 long checkTheMoneyInput(string numcheck);
 string checkThePhoneNumberInput(string numcheck);
 string getPassword();
+void depositFromOutside(Bank &bank, Customer &customer, ofstream &MyFile, ofstream &BankFile);
 #endif FUNCTIONS_H
