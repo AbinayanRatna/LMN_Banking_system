37b8985322479a86a343ee3dfb23a6b0c23aeff6
FathimaNusha <146192217+FathimaNusha@users.noreply.github.com>
2024-01-06 11:08:01 +0530

    functions header

---
+++ Headers/functions.h
@@ -0,0 +1,15 @@
+// T.F. Nusha - 21/ENG/138
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include "Customer.h"
+#include "Bank.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
+// Function declarations (prototypes)
+long checkTheMoneyInput(string numcheck);
+string checkThePhoneNumberInput(string numcheck);
+string getPassword();
+#endif FUNCTIONS_H
